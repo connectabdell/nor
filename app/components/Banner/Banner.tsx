cbeef04c08cf60de235b7668f05c141917ef02c0
connectabdell <85404081+connectabdell@users.noreply.github.com>
2024-05-23 12:18:16 -0600

    update banner

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -9,7 +9,7 @@ const Banner = () => {
                 <div className="mx-auto max-w-7xl pt-16 sm:pt-20 pb-20">
                     <div className="text-center">
                         <h1 className="text-4xl font-semibold text-navyblue sm:text-5xl  lg:text-7xl md:4px lh-96">
-                            iPhone  <br /> integrated agency.
+                            iPhone  <br /> Parts.
                         </h1>
                         <p className="mt-6 text-sm leading-5 text-black">
                             Ehya is the Instagram analytics platform teams use to stay focused on the goals, track <br /> engagement for report your business .
