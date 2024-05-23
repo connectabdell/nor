ca9a55189ad774c9400567831de2524371f8204a
connectabdell <85404081+connectabdell@users.noreply.github.com>
2024-05-23 11:44:32 -0600

    first commit

---
+++ app/components/Banner/Banner.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+
+
+const Banner = () => {
+    return (
+        <main>
+            <div className="px-6 lg:px-8">
+                <div className="mx-auto max-w-7xl pt-16 sm:pt-20 pb-20">
+                    <div className="text-center">
+                        <h1 className="text-4xl font-semibold text-navyblue sm:text-5xl  lg:text-7xl md:4px lh-96">
+                            Solve problem with an <br /> integrated agency.
+                        </h1>
+                        <p className="mt-6 text-sm leading-5 text-black">
+                            Ehya is the Instagram analytics platform teams use to stay focused on the goals, track <br /> engagement for report your business .
+                        </p>
+                    </div>
+                    
+                    
+                </div>
+                
+                    
+            </div>
+
+
+   
+    
+   
+
+
+
+        </main>
+    )
+}
+
+export default Banner;
+
+
---
+++ app/components/Network/index.tsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+
+interface whydata {
+    heading: string;
+    subheading: string;
+}
+
+const whydata: whydata[] = [
+    {
+        heading: "Original OLED  | $299",
+        subheading: "Genuiun Original Screen Display ",
+    },
+    {
+        heading: "Soft OLED | $129 ",
+        subheading: "Aftermarket China OLED ",
+    },
+    {
+        heading: "LCD",
+        subheading: "Copy China Screen ",
+    }
+]
+
+
+const Why = () => {
+    return (
+        <div id="about">
+
+            <div className='mx-auto max-w-7xl px-4 my-20 sm:py-20 lg:px-8'>
+                <div className='grid grid-cols-1 lg:grid-cols-2'>
+
+                    {/* COLUMN-1 */}
+                    <div className="lg:-ml-64">
+                        <Image src="/assets/why/iPad.svg" alt="iPad-image" width={4000} height={900} />
+                    </div>
+
+                    {/* COLUMN-2 */}
+                    <div>
+                        <h3 className="text-4xl lg:text-5xl pt-4 font-semibold sm:leading-tight mt-5 text-center lg:text-start">iPhone 14 Pro Max </h3>
+                        <h4 className="text-lg pt-4 font-normal sm:leading-tight text-center text-black lg:text-start">Dont waste time on search manual tasks. Let Automation do it for you. Simplify workflows, reduce errors, and save time.</h4>
+
+                        <div className="mt-10">
+                            {whydata.map((items, i) => (
+                                <div className="flex mt-4" key={i}>
+                                    <div className="rounded-full h-10 w-12 flex items-center justify-center bg-white">
+                                        
+                                    </div>
+                                    <div className="ml-5">
+                                        <h4 className="text-2xl font-semibold">{items.heading}</h4>
+                                        <h5 className="text-sm text-black font-small mt-2">{items.subheading}</h5>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Why;
